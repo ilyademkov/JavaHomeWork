a702b9c49d3b6f7063d21948a384ffa0f844169c
Илья Демков <zigidrafin1606@gmail.com>
2019-12-09 17:26:00 +0200

    Laba4

---
+++ Lab4/src/Main.java
@@ -0,0 +1,185 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main
+{
+
+  public static void main(String[] args)
+  {
+    Sportsman sportsman = new Sportsman();
+    Person person = new Person();
+    File fileOperator = new File();
+    Scanner in = new Scanner(System.in);
+    int menu;
+    String requiredCountry;
+    String requiredSport;
+    String removeSportsman;
+    fileOperator.Load(person);
+    for (int i = 0; i < person.sportsmen.size(); i++)
+    {
+      System.out.println(person.sportsmen.get(i).toString());
+    }
+    while (true)
+    {
+      System.out.println("Выберите действие: ");
+      System.out.println("1 - Добавление спортсмена");
+      System.out.println("2 - Выдача информации об участниках заданной страны");
+      System.out.println("3 - Выдача информации об участниках по заданному виду спорта");
+      System.out.println("4 - Дисквалификация спортмена по просьбе комиссии");
+      System.out.println("5 - Перечень медалей полученных каждой из стран");
+      System.out.println("6 - Список всех спортсменов");
+      System.out.println("7 - Сохранить список спортсменов");
+
+      System.out.println("0 - Завершить");
+      menu = in.nextInt();
+      switch (menu)
+      {
+        case 1:
+          try
+          {
+            System.out.println("Введите Регистрационный номер, ФИО, Страну, Вид спорта и медаль полученную спортсменом: ");
+            System.out.println("Фио: ");
+            do
+            {
+            } while (!sportsman.setFIO(in.next()));
+            System.out.println("Регистрационный номер: ");
+            do
+            {
+            } while (!sportsman.setRegistNumber(in.nextLong()));
+            System.out.println("Занятое место: ");
+            do
+            {
+            } while (!sportsman.setMedal(in.next()));
+            System.out.println("Страна: ");
+            do
+            {
+            } while (!sportsman.setCountry(in.next()));
+            System.out.println("Вид спорта: ");
+            do
+            {
+            } while (!sportsman.setSport(in.next()));
+            person.sportsmen.add(new Sportsman(sportsman.getFIO(), sportsman.getRegistNumber(), sportsman.getMedal(), sportsman.getCountry(), sportsman.getSport()));
+            fileOperator.Save(person);
+          }
+          catch (InputMismatchException e)
+          {
+            System.out.println("Проверте правильность введенных данных");
+          }
+          break;
+        case 2:
+          if (person.sportsmen.size() > 0)
+          {
+            System.out.println("Введите желаемую страну: ");
+            requiredCountry = in.next();
+            System.out.println("Для выбранной страны - " + requiredCountry + " - представленны следующие призеры: ");
+            int k = 0;
+            for (int i = 0; i < person.sportsmen.size(); i++)
+            {
+              if (person.sportsmen.get(i).getCountry().equalsIgnoreCase(requiredCountry))
+              {
+                System.out.println("Спортсмен №" + (i + 1) + ":" + person.sportsmen.get(i).toString());
+                k++;
+              }
+            }
+            if (k == 0)
+            {
+              System.out.println("Совпадений не найдено");
+            }
+          }
+          else
+          {
+            System.out.println("База данных пуста");
+          }
+          break;
+        case 3:
+          if (person.sportsmen.size() > 0)
+          {
+            System.out.println("Введите желаемый вид спорта: ");
+            requiredSport = in.next();
+            System.out.println("По выбранному виду спорта - " + requiredSport + " - представленны следующие призеры: ");
+            int k = 0;
+            for (int i = 0; i < person.sportsmen.size(); i++)
+            {
+              if (person.sportsmen.get(i).getSport().equalsIgnoreCase(requiredSport))
+              {
+                System.out.println("Спортсмен №" + (i + 1) + ":" + person.sportsmen.get(i).toString());
+                k++;
+              }
+            }
+            if (k == 0)
+            {
+              System.out.println("Совпадений не найдено");
+            }
+          }
+          else
+          {
+            System.out.println("База данных пуста");
+          }
+          break;
+        case 4:
+          if (person.sportsmen.size() > 0)
+          {
+            System.out.println("Введите ФИО спрорстмена для дисквалификации: ");
+            removeSportsman = in.next();
+            boolean k = false;
+            for (int i = 0; i < person.sportsmen.size(); i++)
+            {
+              if (person.sportsmen.get(i).getFIO().equalsIgnoreCase(removeSportsman))
+              {
+                person.sportsmen.remove(i);
+                fileOperator.Save(person);
+                person.sportsmen.clear();
+
+                System.out.println("Спортсмен " + removeSportsman + " был успешно дисквалифицирован");
+                k = true;
+              }
+            }
+            if (!k)
+            {
+              System.out.println("Совпадений не найдено");
+            }
+          }
+          else
+          {
+            System.out.println("База данных пуста");
+          }
+          break;
+        case 6:
+          fileOperator.Load(person);
+          for (int i = 0; i < person.sportsmen.size(); i++)
+          {
+            System.out.println("Спортсмен №" + (i + 1) + ":" + person.sportsmen.get(i).toString());
+          }
+
+          break;
+        case 5:
+          if (person.sportsmen.size() > 0)
+          {
+            int k = 0;
+            for (int i = 0; i < person.sportsmen.size(); i++)
+            {
+
+              System.out.println(person.sportsmen.get(i).getCountry() + " - " + person.sportsmen.get(i).getMedal());
+              k++;
+            }
+            if (k == 0)
+            {
+              System.out.println("Совпадений не найдено");
+            }
+          }
+          else
+          {
+            System.out.println("База данных пуста");
+          }
+          break;
+        case 7:
+          if(person.sportsmen.size() > 0){
+            fileOperator.Save(person);
+          }
+          break;
+        case 0:
+          System.exit(0);
+      }
+    }
+  }
+}
---
+++ Lab4/src/Person.java
@@ -0,0 +1,7 @@
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Person implements Serializable
+{
+  ArrayList<Sportsman> sportsmen = new ArrayList();
+}
