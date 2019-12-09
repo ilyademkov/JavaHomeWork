540ea02a9a2c67392851a46184b7f911c0aff714
Илья Демков <zigidrafin1606@gmail.com>
2019-12-09 17:30:43 +0200

    laba4

---
+++ laba4/src/File.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.ArrayList;
+
+public class File extends Person {
+  private static final String Filename = "C:\\Users\\User\\IdeaProjects\\JavaHomeWork\\laba4\\lab4.txt";
+  public void Load(Person person){
+    try {
+      FileInputStream fin = new FileInputStream(Filename);
+      ObjectInputStream ois = new ObjectInputStream(fin);
+      person.sportsmen = (ArrayList<Sportsman>) ois.readObject();
+      ois.close();
+      fin.close();
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    } catch (ClassNotFoundException e) {
+      e.printStackTrace();
+    }
+  }
+  public void Save(Person person){
+    try{
+      FileOutputStream fileOut = new FileOutputStream(Filename);
+      ObjectOutputStream oos = new ObjectOutputStream(fileOut);
+      oos.writeObject(person.sportsmen);
+      oos.close();
+      fileOut.close();
+     
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
