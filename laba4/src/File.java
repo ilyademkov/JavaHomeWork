683432dfaf89774b84673111137e6a5ef3c86e43
Илья Демков <zigidrafin1606@gmail.com>
2019-12-15 18:37:57 +0200

    troubles

--- laba4/src/File.java
+++ laba4/src/File.java
@@ -2,9 +2,9 @@ import java.io.*;
 import java.util.ArrayList;
 
 public class File extends Person {
-  private static final String Filename = "C:\\Users\\User\\IdeaProjects\\JavaHomeWork\\laba4\\lab4.txt";
   public void Load(Person person){
     try {
+      String Filename = "C:\\Users\\User\\IdeaProjects\\JavaHomeWork\\laba4\\lab4.txt";
       FileInputStream fin = new FileInputStream(Filename);
       ObjectInputStream ois = new ObjectInputStream(fin);
       person.sportsmen = (ArrayList<Sportsman>) ois.readObject();
@@ -20,6 +20,7 @@ public class File extends Person {
   }
   public void Save(Person person){
     try{
+      String Filename = "C:\\Users\\User\\IdeaProjects\\JavaHomeWork\\laba4\\lab4.txt";
       FileOutputStream fileOut = new FileOutputStream(Filename);
       ObjectOutputStream oos = new ObjectOutputStream(fileOut);
       oos.writeObject(person.sportsmen);
